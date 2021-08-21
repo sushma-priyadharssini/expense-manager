efe0f490c40c919f2cb960ae20e09c118476fd34
Sushma Priyadharssini KV <kvsushmapriyadharssini@gmail.com>
2021-08-22 00:58:47 +0530

    feat(expense): Add expense form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/expenses/expenses";
+import NewExpense from "./components/newExpense/newExpense";
 
 function App() {
   const data = [
@@ -7,8 +8,14 @@ function App() {
       { id: '3', title: 'Bus Expense', date: new Date(2021, 2, 19), amount: '₹500'},
       { id: '4', title: 'New Earphones', date: new Date(2020, 2, 6), amount: '₹550'}
   ];
+
+  const addNewExpenseHandler = (newExpense) => {
+    console.log(newExpense);
+  }
+  
   return (
     <div id="app">
+      <NewExpense addNewExpenseHandler={addNewExpenseHandler}/>
       <Expenses data={data}/>
     </div>
   );
---
+++ src/components/newExpense/newExpense.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './newExpense.css'
+import Card from '../ui/card';
+
+const NewExpense = (props) => {
+    const [ userInput, setUserInput ] = useState({
+        title: '',
+        amount: '',
+        date: ''
+    });
+
+    const inputChangeHandler = (inputField, event) => {
+        const obj = {};
+        obj[inputField] = event.target.value;
+
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                ...obj
+            }
+        });
+    };
+
+    const addNewExpenseHandler = () => {
+        props.addNewExpenseHandler({
+            ...userInput,
+            id: Math.random(0, 100)
+        });
+        setUserInput(() => {
+            return {
+                title: '',
+                amount: '',
+                date: ''
+            }
+        });
+    }
+
+    return (
+        <Card className="new-expense-card">
+            <form onSubmit={addNewExpenseHandler}>
+                <div className="expense-controls">
+                    <div className="expense-control">
+                        <label>Title</label>
+                        <input type="text" value={userInput.title} onChange={inputChangeHandler.bind(this, 'title')} />
+                    </div>
+                    <div className="expense-control">
+                        <label>Amount</label>
+                        <input type="text" value={userInput.amount} onChange={inputChangeHandler.bind(this, 'amount')} />
+                    </div>
+                    <div className="expense-control">
+                        <label>Date</label>
+                        <input type="date" value={userInput.date} onChange={inputChangeHandler.bind(this, 'date')} />
+                    </div>
+                </div>
+                <button type="submit">Add Expense</button>
+            </form>
+        </Card>
+    )
+}
+
+export default NewExpense;
